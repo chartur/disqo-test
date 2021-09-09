99fb78b4986750851ce85cd7d0a4f9a72e23e904
Artur Chilingaryan <1995.chilingaryan@gmail.com>
2021-09-09 15:01:55 +0400

    angular project initialized. Also, theming is done as well.

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {HelloWorldComponent} from "./pages/hello-world/hello-world.component";
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HelloWorldComponent
+  }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
